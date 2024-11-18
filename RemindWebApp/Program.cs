a83b5bd1343e8c6eea8f7a6c7cccdc08ae5a083d
mushfig-agahyev-developer <mushfig.aghayev.webdeveloper@gmail.com>
2024-11-19 00:41:45 +0400

    ok

--- RemindWebApp/Program.cs
+++ RemindWebApp/Program.cs
@@ -49,7 +49,6 @@ builder.Services.AddControllersWithViews(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
